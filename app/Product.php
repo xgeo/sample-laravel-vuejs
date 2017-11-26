e7b7991713c159fae0e7090968365962330f9ede
Geovanny L. Coutinho <geovannylc@gmail.com>
2017-11-26 02:03:58 -0300

    cutting the useless and returning the necessary data. Adding new field as fillable

--- app/Product.php
+++ app/Product.php
@@ -7,7 +7,8 @@ use App\ProductCategory;
 
 class Product extends Model
 {
-    protected $fillable = ['name', 'description', 'image', 'price'];
+    protected $fillable = ['name', 'description', 'image', 'price', 'product_categories_id'];
+    protected $hidden  = ['created_at', 'updated_at', 'deleted_at', 'status'];
 
     public function productCategories() {
         return $this->belongsTo(ProductCategory::class, 'product_categories_id');
