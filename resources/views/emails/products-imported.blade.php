14c1421a646a6b5ddff44ded74fd887709a1a764
Geovanny L. Coutinho <geovannylc@gmail.com>
2017-11-28 20:59:09 -0300

    E-mail layout

---
+++ resources/views/emails/products-imported.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <h1>Hello Sir!</h1>
+    <p>Your products have been imported successfully.</p>
+    <hr>
+    <h3>Product list</h3>
+    <blockquote>
+        <pre>
+        {{ $json  }}
+        </pre>
+    </blockquote>
+</div>
