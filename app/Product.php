b3db3e302f1884d7f205eed4b9b84fdce64956b1
Geovanny L. Coutinho <geovannylc@gmail.com>
2017-11-28 21:01:02 -0300

    date fields

--- app/Product.php
+++ app/Product.php
@@ -3,12 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use App\ProductCategory;
 
 class Product extends Model
 {
     protected $fillable = ['name', 'description', 'image', 'price', 'product_categories_id'];
     protected $hidden  = ['created_at', 'updated_at', 'deleted_at', 'status'];
+    protected $dates = ['created_at', 'updated_at'];
 
     public function productCategories() {
         return $this->belongsTo(ProductCategory::class, 'product_categories_id');
